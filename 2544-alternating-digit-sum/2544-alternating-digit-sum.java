406daab30f117574abc0a4538230938b2559b16e
Arunkumarp22 <92368146+Arunkumarp22@users.noreply.github.com>
2023-02-02 20:34:28 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (96.64%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int alternateDigitSum(int n) {
+        String str=Integer.toString(n);
+        int sum=0;
+        for(int i=0;i<str.length();i++){
+            ;
+            if((i+1)%2==1){
+                sum+=str.charAt(i)-'0';
+            }else{
+                sum-=str.charAt(i)-'0';
+            }
+        }
+        return sum;
+    }
+}
